830c5af982ae99bb4042526dd4e8cea2465b5448
Matt Barlow <matt.barlow@rackspace.com>
2018-12-11 14:23:07 -0600

    Remove references to loggregator.

--- tasks/cf_smoke_tests.sh
+++ tasks/cf_smoke_tests.sh
@@ -25,14 +25,6 @@ EOF
 
 cat $CONFIG
 
-loggregator_enabled=${loggregator_enabled:0}
-if [[ "${loggregator_enabled}" == 0 ]]
-then
-  loggregator="--skip=Loggregator"
-else
-  loggregator="--focus=Loggregator:"
-fi
-
 # If there are other apps in the cf org and space, let's just fail
 space_guid=$(cf curl /v2/spaces | jq -r --arg i "${cf_space}" '.resources[] | select(.entity.name == $i) | .metadata.guid')
 if [ -z "$space_guid" ]
@@ -50,7 +42,7 @@ then
 fi
 
 cd /go/src/github.com/cloudfoundry/cf-smoke-tests/
-ginkgo -r --succinct -slowSpecThreshold=300 -v -trace ${loggregator}
+ginkgo -r --succinct -slowSpecThreshold=300 -v -trace
 
 EXITSTATUS=$?
 # halp
@@ -71,14 +63,14 @@ echo "smoke.status curl data: \"series\" :
          [{\"metric\":\"smoke.status\",
           \"points\":[[$currenttime, $cf_smoke_success]],
           \"type\":\"gauge\",
-          \"tags\":[\"deployment:$cf_deployment\",\"loggregator_enabled:$loggregator_enabled\"]"
+          \"tags\":[\"deployment:$cf_deployment\"]"
 
 curl -X POST -H "Content-type: application/json" \
   -d "{ \"series\" :
          [{\"metric\":\"smoke.status\",
           \"points\":[[$currenttime, $cf_smoke_success]],
           \"type\":\"gauge\",
-          \"tags\":[\"deployment:$cf_deployment\",\"loggregator_enabled:$loggregator_enabled\"]
+          \"tags\":[\"deployment:$cf_deployment\"]
         }]
       }" \
 "https://app.datadoghq.com/api/v1/series?api_key=${datadog_key}"
@@ -90,7 +82,7 @@ echo "execution time curl data: \"series\" :
          [{\"metric\":\"smoke.execution_time_ms\",
           \"points\":[[$currenttime, $ELAPSED_TIME]],
           \"type\":\"gauge\",
-          \"tags\":[\"deployment:$cf_deployment\",\"loggregator_enabled:$loggregator_enabled\"]"
+          \"tags\":[\"deployment:$cf_deployment\"]"
 
 
 curl -X POST -H "Content-type: application/json" \
@@ -98,7 +90,7 @@ curl -X POST -H "Content-type: application/json" \
          [{\"metric\":\"smoke.execution_time_ms\",
           \"points\":[[$currenttime, $ELAPSED_TIME]],
           \"type\":\"gauge\",
-          \"tags\":[\"deployment:$cf_deployment\",\"loggregator_enabled:$loggregator_enabled\"]
+          \"tags\":[\"deployment:$cf_deployment\"]
         }]
       }" \
 "https://app.datadoghq.com/api/v1/series?api_key=${datadog_key}"
