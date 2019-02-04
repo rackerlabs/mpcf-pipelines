144e4fa757e2e1ed0fb56e294fcdd7543ada37b5
Matt Barlow <matt.barlow@rackspace.com>
2019-02-04 14:11:03 -0600

    Fix om command.

--- tasks/show_staged_changes.sh
+++ tasks/show_staged_changes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -eu
 
@@ -6,25 +6,26 @@ TEMP_DEPLOYED_PRODUCTS=$(mktemp)
 TEMP_STAGED_PRODUCTS=$(mktemp)
 DEPLOYED_MANIFEST=$(mktemp)
 STAGED_MANIFEST=$(mktemp)
+OM_CMD="om --env env/${ENV_FILE} -k curl --silent --path"
 
-alias om="om --env env/${ENV_FILE}"
+#get jq installed
+apt-get install -y jq
 
 # Get all the deployed products
-om -k curl --silent  --path /api/v0/deployed/products | jq -r .[].guid | grep -v bosh > ${TEMP_DEPLOYED_PRODUCTS}
-om -k curl --silent  --path /api/v0/staged/products | jq -r .[].guid > ${TEMP_STAGED_PRODUCTS}
-
+${OM_CMD} /api/v0/deployed/products | jq -r .[].guid | grep -v bosh > ${TEMP_DEPLOYED_PRODUCTS}
+${OM_CMD} /api/v0/staged/products | jq -r .[].guid > ${TEMP_STAGED_PRODUCTS}
 
 while read i; do
     if grep -q ${i} ${TEMP_DEPLOYED_PRODUCTS}
     then
-	echo "****** $i STAGED CHANGES ******"
-	# Get the deployed manifest given a product guid
-	om -k curl --path /api/v0/deployed/products/${i}/manifest --silent > ${DEPLOYED_MANIFEST}
+        echo "****** $i STAGED CHANGES ******"
+        # Get the deployed manifest given a product guid
+        ${OM_CMD} /api/v0/deployed/products/${i}/manifest > ${DEPLOYED_MANIFEST}
 
-	#Get the staged manifest
-	om -k curl --path /api/v0/staged/products/${i}/manifest --silent | jq .manifest > ${STAGED_MANIFEST}
+        #Get the staged manifest
+        ${OM_CMD} /api/v0/staged/products/${i}/manifest | jq .manifest > ${STAGED_MANIFEST}
 
-	# Diff the files
-	diff --unified=5 ${DEPLOYED_MANIFEST} ${STAGED_MANIFEST} || true
+        # Diff the files
+        diff --unified=5 ${DEPLOYED_MANIFEST} ${STAGED_MANIFEST} || true
     fi
 done < ${TEMP_STAGED_PRODUCTS}
